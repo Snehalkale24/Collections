fec0bc8fb21a72f4542aa290320befddbd49cfd4
Snehalkale24 <snehalbkale2004@gmail.com>
2026-08-03 22:29:14 +0530

    add

---
+++ ex1.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class ex1{
+	public static void main(String args[])
+	{
+		ArrayList jpa=new ArrayList();
+		jpa.add("snehal");
+		jpa.add("sonu");
+		jpa.add(11);
+		jpa.add(11);
+		jpa.add(null);
+		jpa.add(false);
+
+		System.out.println(jpa);
+	}
+}
