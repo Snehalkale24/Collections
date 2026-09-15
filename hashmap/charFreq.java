928a26ba64c26ad70172758b3909e36c6dd98b58
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-15 21:26:18 +0530

    add

--- hashmap/charFreq.java
+++ hashmap/charFreq.java
@@ -1,13 +1,32 @@
+// import java.util.*;
+// public class charFreq{
+// 	public static void main(String args[])
+// 	{
+// 		String str="hello";
+// 		HashMap<Character,Integer>map=new HashMap<>();
+// 		for(int i=0;i<str.length();i++)
+// 		{
+// 			char ch=str.charAt(i);
+// 			if(map.containsKey(ch)){
+// 				map.put(ch,map.get(ch)+1);
+// 			}
+// 			else{
+// 				map.put(ch,1);
+// 			}
+// 		}
+// 		System.out.println(map);
+// 	}
+// }
 import java.util.*;
 public class charFreq{
 	public static void main(String args[])
 	{
-		String str="hello";
+		String str="banana";
 		HashMap<Character,Integer>map=new HashMap<>();
-		for(int i=0;i<str.length();i++)
-		{
-			char ch=str.charAt(i);
-			if(map.containsKey(ch)){
+		for(int i=0;i<str.length();i++){
+			char ch=str.length();
+			if(map.containsKey(ch))
+			{
 				map.put(ch,map.get(ch)+1);
 			}
 			else{
