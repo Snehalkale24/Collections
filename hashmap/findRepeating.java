3f6cdb51e7d9572dcb130a0fdffcd05f23104585
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-15 22:01:45 +0530

    add

--- hashmap/findRepeating.java
+++ hashmap/findRepeating.java
@@ -28,9 +28,9 @@ public class findRepeating{
 		for(int i=0;i<str.length();i++)
 		{
 			char ch=str.charAt(i);
-			if(map.containsKey(ch))
-			{
-				System.out.println(map);
+			if(map.containsKey(ch)){
+				System.out.println(ch);
+				break;
 			}
 			else{
 				map.put(ch,1);
