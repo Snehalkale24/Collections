3c53f63cfb32f081de85f3de9970a407ef3169aa
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-15 21:55:16 +0530

    add

--- hashmap/duplicate.java
+++ hashmap/duplicate.java
@@ -1,37 +1,20 @@
-// import java.util.*;
-// public class duplicate{
-// 	public static void main(String args[])
-// 	{
-// 		int arr[]={1,2,3,23};
-// 		HashMap<Integer,Integer>map=new HashMap<>();
-// 		for(int i=0;i<arr.length;i++)
-// 		{
-// 			if(map.containsKey(arr[i]))
-// 			{
-// 				System.out.println(map);
-// 			}
-// 			else{
-// 				map.put(arr[i],1);
-// 			}
-// 		}
-// 	}
-// }
-
 import java.util.*;
-public class duplicate{
-	public static void main(String args[])
-	{
-		int arr[]={1,2,3,4,5};
-		HashMap<Integer,Integer>map=new HashMap<>();
-		for(int i=0;i<arr.length;i++)
-		{
-			if(map.containsKey(arr[i]))
-			{
-				System.out.println(map);
-			}
-			else{
-				map.put(arr[i],1);
-			}
-		}
-	}
+
+public class duplicate {
+    public static void main(String args[]) {
+
+        int arr[] = {1, 2, 3, 2, 23};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < arr.length; i++) {
+
+            if(map.containsKey(arr[i])) {
+                System.out.println("Duplicate: " + arr[i]);
+            }
+            else {
+                map.put(arr[i], 1);
+            }
+        }
+    }
 }
