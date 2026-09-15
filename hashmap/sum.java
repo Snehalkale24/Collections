3d31eef654a117641dead922c4d077c43e9b063e
Snehalkale24 <snehalbkale2004@gmail.com>
2026-09-15 22:40:44 +0530

    add

--- hashmap/sum.java
+++ hashmap/sum.java
@@ -2,6 +2,12 @@ import java.util.*;
 public class sum{
 	public static void main(String args[])
 	{
-		
+		int arr[]={12,2,3,4};
+		HashMap<Integer,Character>map=new HashMap<>();
+		for(int i=0;i<arr.length;i++)
+		{
+			int sum=sum+no[i];
+		}
+		System.out.println("sum:"+sum);
 	}
 }
